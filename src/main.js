32c202e2f61e74ad07c9d4629d3ff7bb4c54cdc4
alun <alun@bytedance.com>
2023-10-24 15:52:00 +0800

    preload渲染进程脚本

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('node:path');
 
 const a = 12;
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     });
 
     win.loadFile('./config/index.html');
