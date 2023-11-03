4794b6de113306e6782325418bf8fa33be8f0684
alun <alun@bytedance.com>
2023-11-03 14:26:51 +0800

    干掉多余内容

--- src/render/pages/Home.tsx
+++ src/render/pages/Home.tsx
@@ -14,18 +14,8 @@ export const Home: React.FC<HomeProps> = props => {
     const ref = useRef(null);
     return (
         <div>
-            <div onClick={() => {
-                // 动态载入print.ts
-                import('@/components/print').then(res => {
-                    res.default();
-                });
-                setCount(count + 1);
-                console.log(ref.current);
-            }}
-            >
-                test
-                <Container data={count} ref={ref} />
-            </div>
+            hello
+            <Container data={count} ref={ref} />
         </div>
     );
 };
