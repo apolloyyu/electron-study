044af61fd2714c71ac02cf074af960c36e199b87
alun <alun@bytedance.com>
2023-11-03 14:44:35 +0800

    temp

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,9 +1,12 @@
+import { IS_DEV } from '@/config';
 import { app, BrowserWindow, ipcMain, dialog } from 'electron';
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        // titleBarStyle: 'hidden', // 是否展示标题栏
+        // titleBarOverlay: true, // 是否展示最小化 关闭等右上角 mac则需要额外配置
         webPreferences: {
             // preload: path.join(__dirname, 'preload.js')
             // https://github.com/electron/forge/issues/2567 
@@ -13,10 +16,11 @@ const createWindow = () => {
         }
     });
 
-    if (process.env.NODE_ENV === 'production') {
-        win.loadFile('../render/index.html');
-    } else {
+    if (IS_DEV) {
         win.loadURL('http://localhost:8288/output/render/index.html');
+        // win.webContents.openDevTools();
+    } else {
+        win.loadFile('../render/index.html');
     }
 };
 
